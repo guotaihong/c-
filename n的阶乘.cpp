3699ab78f1a7a13319ad443d90b9f6d1e0bd79f1
guotaihong <1320749565@qq.com>
2022-10-08 17:59:14 +0800

    代码

---
+++ n的阶乘.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+
+int main()
+{
+	/*int i = 1;
+	int n = 0;*/
+	//int d = 1;
+	//scanf("%d",&n); 
+	//while (i <= n)   /*注意后面不能加；不然没输出结果*/
+	//{d = d*i;
+	//  i++; 
+	//}
+	//printf("%d", d);
+	
+	
+	
+	
+	/*int ret = 1；
+		sacanf_s("%d", ret);
+	for (i = 1; i <= n; i++) { ret *= i; }
+	printf("%d", ret);
+	return  0;
+}*/
