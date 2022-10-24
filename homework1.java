b21da011b4c45af72f5a811f64efeee9eec50a55
guotaihong <1320749565@qq.com>
2022-10-24 20:54:10 +0800

    homework1

---
+++ homework1.java
@@ -0,0 +1,13 @@
+public class homework1{
+	public static void main(String[]args){
+		int sum = 0,i=0,n=0;
+		for(n=1;n<=100;n++)
+		{
+		for(i=1;i<=n;i++){
+			sum+=i;
+		}
+
+
+		}System.out.println(sum);
+	}
+}
